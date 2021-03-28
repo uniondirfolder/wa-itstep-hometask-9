b089cd59bd90241feb6602a857edf7f7ca7a15a2
NVV <uniondirfolder@gmail.com>
2021-03-28 08:02:07 -0700

    Add model TodoItem

---
+++ wa-itstep-hometask-9/Models/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wa_itstep_hometask_9.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsComplete { get; set; }
+        public string Category { get; set; }
+        public string Lable { get; set; }
+    }
+}
